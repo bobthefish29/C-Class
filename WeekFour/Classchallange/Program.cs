4d293731010386dcac0e352ec26a5812f1a3e4f6
bobthefish29 <dapunchak@email.neit.edu>
2024-04-16 09:22:32 -0400

    EndOfClassW4D1

---
+++ WeekFour/Classchallange/Program.cs
@@ -0,0 +1,54 @@
+﻿
+
+
+namespace Week4_classDemo {
+    class Program {
+        struct Point{
+            public int X;
+            public  int Y;
+            public Point(int x, int y){
+                X = x;
+                Y = y;
+            }
+        }
+        static Point getRandomPoint(){
+            Random rnd = new Random();
+            int xValue = rnd.Next(-101, 101);
+            int yValue = rnd.Next(-101, 101);
+            return new Point(xValue,yValue);
+        }
+        static void Main (string[] args) {
+            Point firstPoint = getRandomPoint();
+            Console.WriteLine($"({firstPoint.X} , {firstPoint.Y})");
+            
+        }
+    }
+
+    
+
+
+
+    class car{
+
+        string color = "red";
+
+        static void Carlists(string[] args){
+
+            
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+
+}
