06c6deafebb34e87de020e38da804c7e7996369a
bobthefish29 <dapunchak@email.neit.edu>
2024-05-16 08:46:51 -0400

    ClassLabAdded

---
+++ Week8/ClassTest/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace gradeCalk{
+    class firstLab{
+        public static int lengthOfLastKey(string UserInputED){
+            int counting = 0;
+            for(int i = UserInputED.Length - 1; i >= 0; i--){
+                if(UserInputED[i] != ' '){
+                    counting++;
+                }else if (counting > 0){
+                    break;
+                }                
+            }
+            return counting;}
+        
+        static void Main (string[] args) {
+            Console.WriteLine("What would you like to input: ");
+            int number = lengthOfLastKey(Console.ReadLine());
+            Console.WriteLine(number);
+        }
+    }
+}
