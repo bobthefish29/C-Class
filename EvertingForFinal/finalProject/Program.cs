75a77d4d0782591f96a10e86f4d3c5b913849315
bobthefish29 <dapunchak@email.neit.edu>
2024-05-21 15:23:12 -0400

    StartedFinal

---
+++ EvertingForFinal/finalProject/Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+
+namespace final{
+
+
+    class Program {
+        
+        class userData{
+            private string userName;
+
+
+            public string USERNAME{
+                get{return userName;}
+                set{ userName = value;}
+            }
+
+            
+
+
+
+        }
+
+        static void Main (string[] args) {
+            string value = "";
+
+            userData temp = new userData();
+
+
+            Console.WriteLine("What would you like to input: ");
+            
+
+            temp.USERNAME = Console.ReadLine();
+            
+            Console.WriteLine(temp.USERNAME);
+        }
+        
+    }
+
+}
